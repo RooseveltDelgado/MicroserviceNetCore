fca6fbbd000a3bccb6223cc9a0a9d027bfc986a5
RooseveltDelgado <dreyluisdel92.4@gmail.com>
2020-10-27 21:14:39 -0500

    api client

---
+++ ApiGateway/ClientApi/Controllers/ClientController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ClientApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClientController : ControllerBase
+    {
+        
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+             return new string[] {"soles","Dolar","Euro" };
+        }
+    }
+}
